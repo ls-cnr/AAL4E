28a1e66373b81f09e773edabf32f1beb2fbcbd3a
Pierfrancesco Martinello <pierfrancesco.martinello@outlook.it>
2022-05-31 11:11:16 +0200

    Added the possibility to change a picture. Comments added as well

--- AFERS/database_handling.py
+++ AFERS/database_handling.py
@@ -1,27 +1,48 @@
+from datetime import datetime, timedelta
 import sqlite3
 import os
+from os.path import exists
 
-class DatabaseHandler:
+#Class for the Database creation and manipulation
+class DataBaseHandler:
 
+    #Define where to store the data. If the database does not exist, it gets created and then a connection is made
     def __init__(self):
-        #Where to store our database
+        #Where to store our data
         file = os.getcwd() + '/AFERS/DB/elder.db'
 
-        try:
-            #Try creating the Database
-            open(file, 'a').close()
-        except OSError:
-            print('Failed creating the file')
+        #If the file does not exist, ...
+        if(exists(file) != True):
+            try:
+                #...try creating it
+                open(file, 'a').close()
+            
+                #Connect to the DB
+                self.connection = sqlite3.connect(file)
 
-        #Connect to the DB
-        self.connection = sqlite3.connect(file)
+                #Create a cursor
+                self.cursor = self.connection.cursor()
 
-        #Create a cursor
-        self.cursor = self.connection.cursor()
+                #Initialize the database by creating the tables
+                self.DBHFirstInit()
+
+            #if the creation process encounters a problem, print the following error  
+            except OSError:
+                print('Failed creating the file')
+
+        #If the file esists, just establish a connection with it
+        else:
+                #Connect to the DB
+                self.connection = sqlite3.connect(file)
+
+                #Create a cursor
+                self.cursor = self.connection.cursor()
 
 
+    #Definitions of the queries to create the tables of our database with their relations
     def DBHFirstInit(self):
-        #Table creation
+
+        #Creation of the table Elders
         self.cursor.execute("""
                             CREATE TABLE Elders (
                                 name TEXT NOT NULL,
@@ -31,6 +52,7 @@ class DatabaseHandler:
                             );
                             """)
 
+        #Creation of the table Moods
         self.cursor.execute("""
                             CREATE TABLE Moods (
                                 elder INTEGER NOT NULL,
@@ -48,82 +70,159 @@ class DatabaseHandler:
                             )
                             """)
 
-        #Committing
+        #Committing the changes through the connection
         self.connection.commit()
 
 
+    #Definition of the query to insert a new person in the Elder table
     def DBHElderlyCommit(self, name, surname, picture):
+        
+        #Query to the database
         self.cursor.execute("INSERT INTO Elders (name, surname, picture_location) VALUES ('{}' , '{}', '{}')".format(name.capitalize(), surname.capitalize(), picture))
 
+
+        #Committing the change through the connection
         self.connection.commit()
 
 
-    def DBHRecognitionCommit(self, name, surname, acquisitionTime, mood, angry, disgust, fear, happiness, sad, surprise, neutral):
+    #Definition of the query to insert a new emotion detection
+    def DBHDetectionCommit(self, name, surname, mood, angry, disgust, fear, happiness, sad, surprise, neutral, acquisitionTime=None):
+
+        #Get the id relative to the person whose emotions must be inserted
         elder = self.DBHGetProgressiveID(name, surname)
+        
+        #In case if the acquisition time is not specified...
+        if acquisitionTime is None:
+            #... Take the actual time and format it
+            acquisitionTime = datetime.now()
+            acquisitionTime.strftime("%m-%d-%Y, 00:00:00")
+
+        #Insert query
         self.cursor.execute("INSERT INTO Moods VALUES ({} , '{}', '{}', {}, {}, {}, {}, {}, {},{})".format(elder, acquisitionTime, mood, angry, disgust, fear, happiness, sad, surprise, neutral))
 
+        #Committing the change through the connection
         self.connection.commit()
 
 
+    #Definition of the query to fetch the progressive number relative to a person
     def DBHGetProgressiveID(self, name,surname):
+
+        #Query
         self.cursor.execute("""
                             SELECT rowid FROM Elders
                             WHERE name = "{}" AND surname = "{}"
                         """.format(name.capitalize(), surname.capitalize()))
         
-        
+        #Returning the value of the id
+        #The function fetchall returns a list of tuple, so [0][0] is necessary to access the first (unique, in this case) value
         return self.cursor.fetchall()[0][0]
 
 
+    #Definition of the query to fetch the picture location relative to a person
     def DBHGetPicture(self, name, surname):
+
+        #Query
         self.cursor.execute("""
                             SELECT picture_location FROM Elders
                             WHERE name = "{}" AND surname = "{}"
                         """.format(name.capitalize(), surname.capitalize()))
         
-        
+        #Returning the location of the picture
+        #The function fetchall returns a list of tuple, so [0][0] is necessary to access the first (unique, in this case) value
         return self.cursor.fetchall()[0][0]
 
 
+    #Definition of the query to get the last emotions detection relative to a person
     def DBHGetLastAcquisition(self, name, surname):
-        id = self.DBHGetProgressiveID(name, surname)
-        self.cursor.execute(""" SELECT acquisition_time, mood, angry, disgust, fear, happiness, sad, surprise, neutral FROM Moods
-                                WHERE elder == {}
-                                ORDER BY acquisition_time DESC
-                            """.format(id))
         
-        return self.cursor.fetchone()
+        #The function simply calls a more general function
+        return self.DBHGetLastNAcquisition(name, surname, 1)
 
     
+    #Definition of the query to get the last N emotions detections relative to a person
     def DBHGetLastNAcquisition(self, name, surname, n):
+
+        #Get the id relative to a person
         id = self.DBHGetProgressiveID(name, surname)
+
+        #Query to fecth all the detections relative to a person
         self.cursor.execute(""" SELECT acquisition_time, mood, angry, disgust, fear, happiness, sad, surprise, neutral FROM Moods
                                 WHERE elder == {}
                                 ORDER BY acquisition_time DESC
                             """.format(id))
         
+        #Returning only the last n acquisitions
         return self.cursor.fetchmany(size=n)
 
 
+    #Definition of the query to get all the detections of the last N days
+    def DBHGetLastNDaysAcquisitions(self, name, surname, n):
+
+        #Get today's date
+
+        time = datetime.today()
+
+        #If n is not zero, subtract n days from "time"
+        if n != 0:
+            time = time - timedelta(days=n)
+
+        #Format the time value
+        date_time = time.strftime("%m-%d-%Y, 00:00:00")
+
+        #Get the id of the person whose emotions data we want to fecth
+        id = self.DBHGetProgressiveID(name, surname)
+
+        #Query
+        self.cursor.execute("""
+                                SELECT acquisition_time, mood, angry, disgust, fear, happiness, sad, surprise, neutral FROM Moods
+                                WHERE elder = {} AND acquisition_time >='{}'
+                                ORDER BY acquisition_time ASC
+        """.format(id, date_time))
+
+        #Return all values as a list of tuples
+        return self.cursor.fetchall()
+
+
+    #Definition of the query to get all the daily emotion detections
+    def DBHGetDailyAcquisitions(self, name, surname):
+
+        #This function calls a more general function
+        return self.DBHGetLastNDaysAcquisitions(name, surname, 0)
+
+
+    #Definition of the query to update the picture location
+    def DBHUpdatePicture(self, name, surname, picture):
+
+        #Query
+        self.cursor.execute("""
+                                UPDATE Elders
+                                SET picture_location = '{}'
+                                WHERE name = '{}' AND surname = '{}'
+                            """.format(picture, name.capitalize(), surname.capitalize()))
+
+
+        #Committing the change through the connection
+        self.connection.commit()
+    
+
+    #Definition of the query to check if a particular entry in the Elder table esists
+    def DBHElderExists(self, name, surname):
+        
+        #Query
+        self.cursor.execute("""
+                                SELECT EXISTS(
+                                    SELECT  rowid
+                                    FROM Elders
+                                    WHERE name = '{}' AND surname = '{}'
+                                )
+                            """.format(name, surname))
+        
+        #Returning the result of the query as a simple binary value
+        return self.cursor.fetchone()[0]
+    
+
+    #Connection closing
     def DBHClose(self):
-        #Close connection
+
+        #Close the connection
         self.connection.close()
-
-
-d = DatabaseHandler()
-d.DBHFirstInit()
-d.DBHElderlyCommit("Pierfra","Me", "ciao")
-d.DBHRecognitionCommit("Pierfra", "me", "Mai", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "Mai più", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "er", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "kjhgf", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "sdfu", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "jhgf", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "oiuygf", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "pojhg", "Hulk", 5,4,3,2,1,0,8)
-d.DBHRecognitionCommit("Pierfra", "me", "wertyui", "Hulk", 5,4,3,2,1,0,8)
-
-ret = d.DBHGetLastNAcquisition("Pierfra", "me", 8)
-for row in ret:
-    print(row)
-d.DBHClose()
